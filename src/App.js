7ff92c1718aec8cf09ec8fd92568fd1a95e4c817
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-10-01 22:40:01 +0600

    hero icon added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar></NavBar>
       <h1 className="text-3xl font-bold underline">This is a Header</h1>
       <p>This is a paragraph</p>
     </div>
   );
 }
-
 export default App;
