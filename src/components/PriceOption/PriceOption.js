eb5ab53d878ba97cec158f6d7366797729b1c0a6
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-10-02 01:28:06 +0600

    price features added

--- src/components/PriceOption/PriceOption.js
+++ src/components/PriceOption/PriceOption.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import {CheckCircleIcon} from '@heroicons/react/24/solid'
+import Feature from '../Feature/Feature';
 
 const PriceOption = ({ option }) => {
+  const { features } = option;
   return (
-    <div className=" bg-indigo-300 m-3 rounded-md p-3">
+    <div className="bg-indigo-300 m-3 rounded-md p-3">
       <div>
         <h3>
           <span className="text-6xl font-bold text-white">{option.price}</span>
-          <span className="text-2xl text-gray-200">/month</span>
+          <span className="text-2xl text-gray-200">/mon</span>
         </h3>
-        <p className="text-3xl my-4" >{option.name}</p>
+        <p className="text-3xl my-4">{option.name}</p>
       </div>
-      <div className='flex items-center'>
-        <CheckCircleIcon className='w-4 h-4 text-white'/>
-        <p className='ml-2'></p>
-
-      </div>
+      {features.map((feature, idx) => (
+        <Feature key={idx} feature={feature}></Feature>
+      ))}
+      <button className="bg-green-500 mt-4 w-full rounded-md py-2 text-white font-bold">
+        Buy Now
+      </button>
     </div>
   );
 };
--- src/components/Pricing/Pricing.js
+++ src/components/Pricing/Pricing.js
@@ -12,7 +12,7 @@ const Pricing = () => {
         'Extra Feature',
         'Unnecessary Feature',
         'Will never use Feature',
-        'Hudai Feature',
+        'Hudai feature',
         'Ajaira Feature',
       ],
     },
@@ -21,11 +21,11 @@ const Pricing = () => {
       name: 'Medium',
       price: 9.99,
       features: [
-        'Awesome Feature',
+        'Everything on Free',
         'Extra Feature',
         'Unnecessary Feature',
         'Will never use Feature',
-        'Hudai Feature',
+        'Hudai feature',
         'Ajaira Feature',
       ],
     },
@@ -34,21 +34,21 @@ const Pricing = () => {
       name: 'Premium',
       price: 19.99,
       features: [
-        'Awesome Feature',
+        'Everything on Premium',
         'Extra Feature',
         'Unnecessary Feature',
         'Will never use Feature',
-        'Hudai Feature',
+        'Hudai feature',
         'Ajaira Feature',
       ],
     },
   ];
   return (
     <div>
-      <h2 className="text-5xl font-bold bg-indigo-300 p-12 text-white ">
-        Best deal of the Town
+      <h2 className="text-5xl font-bold bg-indigo-300 p-12 text-white">
+        Best Deal of the Town
       </h2>
-      <div className="grid grid-cols-3 gap-3">
+      <div className="grid md:grid-cols-3 gap-3 mt-4">
         {pricingOptions.map((option) => (
           <PriceOption key={option.id} option={option}></PriceOption>
         ))}
