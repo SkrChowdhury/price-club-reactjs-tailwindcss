30f5bb49ecfe0f06759f239219408607699c66cc
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-10-02 22:41:46 +0600

    axios practice done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import AssignmentMarks from './components/AssignmentMarks/AssignmentMarks';
 import NavBar from './components/NavBar/NavBar';
+import PhoneBar from './components/PhoneBar/PhoneBar';
 import Pricing from './components/Pricing/Pricing';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <p>This is a paragraph</p>
       <Pricing></Pricing>
       <AssignmentMarks></AssignmentMarks>
+      <PhoneBar></PhoneBar>
     </div>
   );
 }
