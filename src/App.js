f650fd8594776bc5e9bc83851e16f166e994f542
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-10-02 21:38:50 +0600

    recharts added in react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import AssignmentMarks from './components/AssignmentMarks/AssignmentMarks';
 import NavBar from './components/NavBar/NavBar';
 import Pricing from './components/Pricing/Pricing';
 
@@ -10,6 +11,7 @@ function App() {
       <h1 className="text-3xl font-bold underline">This is a Header</h1>
       <p>This is a paragraph</p>
       <Pricing></Pricing>
+      <AssignmentMarks></AssignmentMarks>
     </div>
   );
 }
